9afe51b159c7f9102b785f203a526f67f2f60839
Jonus Cottrell <Jonus@Jonuss-MacBook-Pro.local>
2019-01-19 11:37:10 -0500

    ConstraintsUpdated1

--- DankDelivery/app/src/main/java/com/example/dankdelivery/Address_Page.java
+++ DankDelivery/app/src/main/java/com/example/dankdelivery/Address_Page.java
@@ -1,11 +1,13 @@
 package com.example.dankdelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Address_Page extends AppCompatActivity {
 
@@ -14,16 +16,6 @@ public class Address_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address__page);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);}}
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
-}
