75827a25c7846795dc7cebced6dbfaea8ea2f290
alexlarson98 <larso140@msu.edu>
2019-01-19 15:41:15 -0500

    Changes to Information Strain

--- DankDelivery/app/src/main/java/com/example/dankdelivery/StrainInformation.java
+++ DankDelivery/app/src/main/java/com/example/dankdelivery/StrainInformation.java
@@ -1,5 +1,6 @@
 package com.example.dankdelivery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,9 +8,9 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class StrainInformation extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,7 @@ public class StrainInformation extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+<<<<<<< Updated upstream
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         ActionBar actionBar = getSupportActionBar();
@@ -24,3 +26,32 @@ public class StrainInformation extends AppCompatActivity {
 
             }
         }
+=======
+//        Button btn_Checkout = (Button)findViewById(R.id.checkout);
+//        btn_Checkout.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                openCheckout();
+//            }
+//
+//        });
+
+//      textView_about.setMovementMethod(new ScrollingMovementMethod());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+//    public void openCheckout(){
+//        Intent intent = new Intent(this,Checkout.class);
+//        startActivity(intent);
+//    }
+}
+>>>>>>> Stashed changes
