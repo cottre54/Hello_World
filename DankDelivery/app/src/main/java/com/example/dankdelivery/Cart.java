0df2fb50cc4e4555c5b8fc8f001948b00d06391c
Neil Fraylick <nfraylick@ltu.edu>
2019-01-19 20:07:19 -0500

    new button on cart

--- DankDelivery/app/src/main/java/com/example/dankdelivery/Cart.java
+++ DankDelivery/app/src/main/java/com/example/dankdelivery/Cart.java
@@ -3,6 +3,7 @@ package com.example.dankdelivery;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -16,7 +17,10 @@ public class Cart extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Check");
     }
 
 }
