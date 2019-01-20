dd72658b3c7ae084cc0f36221baf354d42cc3f1d
Kelly Hancox <kellyhancox@Kelly-Hancox-Rules.local>
2019-01-20 10:48:16 -0500

    creating information pages

--- DankDelivery/app/src/main/java/com/example/dankdelivery/GSCInformation.java
+++ DankDelivery/app/src/main/java/com/example/dankdelivery/GSCInformation.java
@@ -2,19 +2,18 @@ package com.example.dankdelivery;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class StrainInformation extends AppCompatActivity {
+public class GSCInformation extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_strain_information);
+        setContentView(R.layout.activity_GSC_information);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -39,5 +38,5 @@ public class StrainInformation extends AppCompatActivity {
         startActivity(intent);
     }
 
-}
 
+}
--- DankDelivery/app/src/main/java/com/example/dankdelivery/SdInformation.java
+++ DankDelivery/app/src/main/java/com/example/dankdelivery/SdInformation.java
@@ -2,20 +2,17 @@ package com.example.dankdelivery;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
-public class StrainInformation1 extends AppCompatActivity {
-
+public class SdInformation extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_strain_information1);
+        setContentView(R.layout.activity_sd_information);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -40,5 +37,5 @@ public class StrainInformation1 extends AppCompatActivity {
         startActivity(intent);
     }
 
-
 }
+
